587d2ba45e9c159b10c542b6d7910b659011b750
zzxy19 <zy112499>
2018-02-13 15:55:08 -0800

    Add second activities that display a message fetched from first activity.

---
+++ app/src/main/java/com/projectz/appz/GreetingActivity.java
@@ -0,0 +1,24 @@
+package com.projectz.appz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.projectz.appz.common.KeyConstants;
+
+public class GreetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greeting);
+
+        Intent intent = getIntent();
+        String message = "Welcome: " + intent.getStringExtra(KeyConstants.GREETING_STRING);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.greeting_text);
+        textView.setText(message);
+    }
+}
---
+++ app/src/main/java/com/projectz/appz/common/KeyConstants.java
@@ -0,0 +1,11 @@
+package com.projectz.appz.common;
+
+/**
+ * Key name constants for passing data between intents.
+ */
+
+public class KeyConstants {
+    private KeyConstants() {}
+
+    public static String GREETING_STRING = "greeting_string";
+}
